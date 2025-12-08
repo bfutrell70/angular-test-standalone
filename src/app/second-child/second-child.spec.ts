b356a00b47070395efbfa8d8cd1feac2da8e91d3
Brian Futrell <bfutrel@gmail.com>
2025-12-08 14:07:21 -0500

    wrote unit test for the SecondChild component

--- src/app/second-child/second-child.spec.ts
+++ src/app/second-child/second-child.spec.ts
@@ -1,16 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SecondChild } from './second-child';
+import { Person } from '../interfaces/person';
+import { By } from '@angular/platform-browser';
 
 describe('SecondChild', () => {
   let component: SecondChild;
   let fixture: ComponentFixture<SecondChild>;
+
+  let person: Person;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SecondChild]
     })
     .compileComponents();
+
+    person = {
+      name: "Test Person",
+      height: "100",
+      mass: "",
+      hair_color: "",
+      skin_color: "",
+      eye_color: "",
+      birth_year: "",
+      gender: "",
+      homeworld: "",
+      films: [
+        "film 1",
+        "film 2"
+      ],
+      species: [],
+      vehicles: [],
+      starships: [],
+      created: new Date(2025, 3, 10),
+      edited: new Date(2025, 3, 10),
+      url: ""
+    };
 
     fixture = TestBed.createComponent(SecondChild);
     component = fixture.componentInstance;
@@ -20,4 +46,11 @@ describe('SecondChild', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the correct number of films', () => {
+    component.Person = person;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('span').textContent).toContain(person.films.length.toString());
+  })
 });
