4100223246477704f8dc35a9df44e93ff0faa820
Brian Futrell <bfutrel@gmail.com>
2025-12-09 10:04:56 -0500

    added test data, wrote unit test

--- src/app/first-child/first-child.spec.ts
+++ src/app/first-child/first-child.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FirstChild } from './first-child';
+import { Person } from '../interfaces/person';
 
 describe('FirstChild', () => {
   let component: FirstChild;
   let fixture: ComponentFixture<FirstChild>;
+
+  let person: Person;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +18,40 @@ describe('FirstChild', () => {
     fixture = TestBed.createComponent(FirstChild);
     component = fixture.componentInstance;
     await fixture.whenStable();
+
+    person = {
+      name: "Test Person",
+      height: "100",
+      mass: "",
+      hair_color: "",
+      skin_color: "",
+      eye_color: "",
+      birth_year: "",
+      gender: "",
+      homeworld: "",
+      films: [
+        "film 1",
+        "film 2"
+      ],
+      species: [],
+      vehicles: [],
+      starships: [],
+      created: new Date(2025, 3, 10),
+      edited: new Date(2025, 3, 10),
+      url: ""
+    };
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // tests
+  // - given a Person object, a div is rendered with the person's name
+  it('renders the name', () => {
+    component.Person = person;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('div').textContent).toContain(person.name);
+  })
 });
